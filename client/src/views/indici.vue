5b13abab5b0bf3dd720d48db7c8f48a93275a84e
Frenz86 <frenzy86@gmail.com>
2021-01-24 00:37:34 +0100

    ww

--- client/src/views/indici.vue
+++ client/src/views/indici.vue
@@ -5,33 +5,70 @@
 </style>
 <template>
   <div>
-    <b-container class="mb-3 justify-content-center" style="max-width: 740px;">
-      <h4 style="margin: 0px;">
-       Qui tabella indici
-      </h4>
-      <h4>{{this.baby}}</h4>
+    <b-container class="mb-3 justify-content-center" style="max-width: 800px;">
+      <h5 style="margin: 0px;">
+
+      <div>
+        <b-tabs content-class="mt-3">
+          <b-tab title="Riassunto" active>
+            <p>Tabella riassunto
+   
+            </p></b-tab>
+          <b-tab title="Bilancio">
+            <p>Tabella bilancio
+            <div>
+              <b-navbar toggleable="lg" type="dark" variant="info">
+                <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                  <b-navbar-nav>
+                    <b-nav-item href="#">Link</b-nav-item>
+                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                  </b-navbar-nav>
+
+                  <!-- Right aligned nav items -->
+                  <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                      <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                      <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+
+                    <b-nav-item-dropdown text="Lang" right>
+                      <b-dropdown-item href="#">EN</b-dropdown-item>
+                      <b-dropdown-item href="#">ES</b-dropdown-item>
+                      <b-dropdown-item href="#">RU</b-dropdown-item>
+                      <b-dropdown-item href="#">FA</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                    <b-nav-item-dropdown right>
+                      <!-- Using 'button-content' slot -->
+                      <template #button-content>
+                        <em>User</em>
+                      </template>
+                      <b-dropdown-item href="#">Profile</b-dropdown-item>
+                      <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                  </b-navbar-nav>
+                </b-collapse>
+              </b-navbar>
+            </div>            
+            
+            
+            </p></b-tab>
+          <b-tab title="Indici"><p>Tabella Indici</p></b-tab>
+          <b-tab title="Medie"><p>Tabella medie</p></b-tab>
+          <b-tab title="Multipli e parametri"><p>Tabella Multipli e parametri</p></b-tab>
+          <b-tab title="Valuation"><p>Formule Valuation</p></b-tab>
+
+        </b-tabs>
+      </div>
+      
+      </h5>
     </b-container>
   </div>
 </template>
 <script>
-import axios from 'axios';
 
-export default {
-  data() {
-    return {
-      baby: "hello",
-    }
-  },
-  methods: {
-    getDisclaimerText() {
-      const text = `
-      This Website is providing the Service of calculating the IOL power using the Hoffer® QST 
-      Formula and the Naeser/Savini Toric Calculator.
-      We, the authors of these formulas and this website assume no responsibility for the clinical
-      outcomes of any given patient. The choice of IOL power for any patient is the sole responsibility
-      of the individual surgeon's clinical judgment.`;
-      return text;
-    },
-  }
-}
 </script>
--- client/src/views/riassunto.vue
+++ client/src/views/riassunto.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="riassunto">
-    <h1>This is an admin page aaa
-      efewfewfw fdsfdsf
-    </h1>
+    <h2>This is an admin page aaa
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+
+    </h2>
   </div>
 </template>
